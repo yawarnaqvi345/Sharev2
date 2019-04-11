746a6c91dbfe7ec20902cff0a9703aa346ae843f
Yawar <yawarnaqvi345@gmail.com>
2019-04-11 13:26:45 +0500

    added send activity fragments

--- app/src/main/java/com/example/share/SendActivity.java
+++ app/src/main/java/com/example/share/SendActivity.java
@@ -1,8 +1,12 @@
 package com.example.share;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.example.share.SendActivityFragments.ViewPagerAdapter;
 
 public class SendActivity extends AppCompatActivity {
     final  String TAG="SendActivity";
@@ -12,5 +16,10 @@ public class SendActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"OnCreate");
         setContentView(R.layout.activity_send);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.send_activity_view_pager);
+       ViewPagerAdapter adapter = new ViewPagerAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.send_activity_tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/example/share/SendActivityFragments/ViewPagerAdapter.java
@@ -0,0 +1,65 @@
+package com.example.share.SendActivityFragments;
+
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.View;
+
+import com.example.share.R;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    Fragment apps,files,photos,videos,music;
+    private Context mContext;
+    TabLayout.Tab tabLayout;
+    public ViewPagerAdapter(Context cntxt,FragmentManager fm) {
+        super(fm);
+        mContext=cntxt;
+        apps=new Apps();
+        files=new Files();
+        photos=new Photos();
+        videos=new Videos();
+        music=new Music();
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch(i){
+            case 0:
+                return apps;
+            case 1:
+                return files;
+            case 2:
+                return photos;
+            case 3:
+                return videos;
+            case 4:
+                return music;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.String_Apps);
+            case 1:
+                return mContext.getString(R.string.String_Files);
+            case 2:
+                return mContext.getString(R.string.String_photos);
+            case 3:
+                return mContext.getString(R.string.String_videos);
+            case 4:
+                return mContext.getString(R.string.String_nusic);
+            default:
+                return null;
+        }
+    }
+}
