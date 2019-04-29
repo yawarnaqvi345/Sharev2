9f528db89caad0e74f7f2e61f45b02743cccb32d
Yawar <yawarnaqvi345@gmail.com>
2019-04-29 18:03:03 +0500

    gms.nearby api application, file transferred but extensiion and name not transfertrred

--- app/src/main/java/com/example/share/FinalShareActivity.java
+++ app/src/main/java/com/example/share/FinalShareActivity.java
@@ -1,5 +1,12 @@
 package com.example.share;
 
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,30 +21,73 @@ import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.ConnectionInfo;
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
 import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
 import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
 import com.google.android.gms.nearby.connection.DiscoveryOptions;
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FinalShareActivity extends AppCompatActivity {
     ConstraintLayout view;
+    Activity mActivity=this;
+    TextView finalshareTextView;
+    File fileToSend;
+     List<FileToSendPath> mPathsList;
+     Payload filePayload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_share);
-        view = findViewById(R.id.mainlayout);
+        mPathsList=SendActivity.mPathsList;
+        Uri uri= Uri.fromFile(new File(mPathsList.get(0).path));
+        try {
+            ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(uri, "r");
+            filePayload = Payload.fromFile(pfd);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        finalshareTextView=findViewById(R.id.finalshare_activity_text);
+     //   view = findViewById(R.id.mainlayout);
         // AdvertisingIdClient advertiserEndpointId;
         EndpointDiscoveryCallback mEndpointDiscoveryCallback = new EndpointDiscoveryCallback() {
             @Override
             public void onEndpointFound(String s, DiscoveredEndpointInfo discoveredEndpointInfo) {
-                Toast.makeText(getApplicationContext(), "onEndpointFound", Toast.LENGTH_SHORT).show();
+
                 //   Snackbar.make(view,"onEndpointFound",Snackbar.LENGTH_SHORT).show();
+                Nearby.getConnectionsClient(mActivity).requestConnection(discoveredEndpointInfo.getEndpointName()
+                        ,discoveredEndpointInfo.getServiceId(),mConnectionLifecycleCallback);
                 Nearby.getConnectionsClient(getApplicationContext())
                         .requestConnection(
                                 /* endpointName= */ discoveredEndpointInfo.getEndpointName(),
-                                discoveredEndpointInfo.getServiceId(),
-                                mConnectionLifecycleCallback);
+                                s,
+                                mConnectionLifecycleCallback).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "on Success connection", Toast.LENGTH_SHORT).show();
+                        Nearby.getConnectionsClient(mActivity)
+                                .stopDiscovery();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "on faliure connection", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Toast.makeText(getApplicationContext(), "onEndpointFound", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -61,11 +111,33 @@ public class FinalShareActivity extends AppCompatActivity {
         @Override
         public void onConnectionInitiated(String s, ConnectionInfo connectionInfo) {
             Toast.makeText(getApplicationContext(), "onConnectionInitiated", Toast.LENGTH_SHORT).show();
+
+            Nearby.getConnectionsClient(getApplicationContext()).acceptConnection(s, mPayLoadCallback);
         }
 
         @Override
         public void onConnectionResult(String s, ConnectionResolution connectionResolution) {
             Toast.makeText(getApplicationContext(), "onConnectionResult", Toast.LENGTH_SHORT).show();
+            switch (connectionResolution.getStatus().getStatusCode()) {
+                case ConnectionsStatusCodes.STATUS_OK:
+                   finalshareTextView.setText("connected to "+s);
+
+                    Nearby.getConnectionsClient(getApplicationContext()).sendPayload(s, filePayload);
+                    Toast.makeText(getApplicationContext(), "Connection Accepted", Toast.LENGTH_SHORT).show();
+                    break;
+                case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                    finalshareTextView.setText("connection to "+s+" rejected");
+                    // The connection was rejected by one or both sides.
+                    Toast.makeText(getApplicationContext(), "Rejected Connection", Toast.LENGTH_SHORT).show();
+                    break;
+                case ConnectionsStatusCodes.STATUS_ERROR:
+                    finalshareTextView.setText("There was an error connecting to "+s);
+                    // The connection broke before it was able to be accepted.
+                    Toast.makeText(getApplicationContext(), "Rejected Connection", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    // Unknown status code
+            }
         }
 
         @Override
@@ -73,5 +145,17 @@ public class FinalShareActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "onDisconnected", Toast.LENGTH_SHORT).show();
         }
     };
+    PayloadCallback mPayLoadCallback=new PayloadCallback() {
+        @Override
+        public void onPayloadReceived(String s, Payload payload) {
+            Toast.makeText(getApplicationContext(),"Payload Received",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPayloadTransferUpdate(String s, PayloadTransferUpdate payloadTransferUpdate) {
+            Toast.makeText(getApplicationContext(),"Payload Updated",Toast.LENGTH_SHORT).show();
+        }
+    };
+
 
 }
--- app/src/main/java/com/example/share/ReceiveActivity.java
+++ app/src/main/java/com/example/share/ReceiveActivity.java
@@ -1,7 +1,9 @@
 package com.example.share;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.IntentFilter;
 import android.database.DataSetObserver;
 import android.net.wifi.WifiManager;
@@ -9,6 +11,7 @@ import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.annotation.NonNull;
 import android.support.v4.text.PrecomputedTextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -23,11 +26,16 @@ import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.share.wifidirect.WiFiDirectBroadcastReceiver;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.ConnectionInfo;
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
 import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
 
 import java.util.Collection;
@@ -40,6 +48,7 @@ public class ReceiveActivity extends AppCompatActivity {
     LottieAnimationView animationView;
     ListView receiveListView;
     Collection<WifiP2pDevice> list;
+    TextView textView;
     //LinearLayout listRecycler= findViewById()
 
     IntentFilter intentFilter;
@@ -47,6 +56,8 @@ public class ReceiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
+        textView=findViewById(R.id.receive_activity_text);
+        animationView=findViewById(R.id.receive_animation_view);
 
       /*  WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         if(wifiManager.getWifiState()==WifiManager.WIFI_STATE_DISABLED)
@@ -75,24 +86,6 @@ public class ReceiveActivity extends AppCompatActivity {
             }
         });*/
 
-
-        ConnectionLifecycleCallback mConnectionLifecycleCallback=new ConnectionLifecycleCallback() {
-            @Override
-            public void onConnectionInitiated(String s, ConnectionInfo connectionInfo) {
-                Toast.makeText(getApplicationContext(),"onConnectionInitiated",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onConnectionResult(String s, ConnectionResolution connectionResolution) {
-                Toast.makeText(getApplicationContext(),"onConnectionResult",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onDisconnected(String s) {
-
-            }
-        };
-
         Nearby.getConnectionsClient(this)
                 .startAdvertising(
                         /* endpointName= */ android.os.Build.MODEL,
@@ -111,6 +104,80 @@ public class ReceiveActivity extends AppCompatActivity {
         super.onPause();
        // unregisterReceiver(receiver);
     }
+    ConnectionLifecycleCallback mConnectionLifecycleCallback=new ConnectionLifecycleCallback() {
+        @Override
+        public void onConnectionInitiated(final String s, ConnectionInfo connectionInfo) {
+            Toast.makeText(getApplicationContext(),"onConnectionInitiated",Toast.LENGTH_SHORT).show();
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ReceiveActivity.this);
+            alertDialogBuilder.setMessage(s+" wants to connect with this device");
+                    alertDialogBuilder.setPositiveButton("Allow",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+                                    Nearby.getConnectionsClient(getApplicationContext()).acceptConnection(s, mPayLoadCallback);
+                                    Toast.makeText(ReceiveActivity.this,"You clicked yes button",Toast.LENGTH_LONG).show();
+                                }
+                            });
 
+            alertDialogBuilder.setNegativeButton("Decline",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Nearby.getConnectionsClient(getApplicationContext()).rejectConnection(s);
+                    finish();
+                }
+            });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            Nearby.getConnectionsClient(ReceiveActivity.this)
+                    .stopAdvertising();
+          //  Nearby.getConnectionsClient(getApplicationContext()).acceptConnection(s, mPayLoadCallback);
+        }
+
+        @Override
+        public void onConnectionResult(String s, ConnectionResolution connectionResolution) {
+            Toast.makeText(getApplicationContext(),"onConnectionResult",Toast.LENGTH_SHORT).show();
+            switch (connectionResolution.getStatus().getStatusCode()) {
+                case ConnectionsStatusCodes.STATUS_OK:
+                    Toast.makeText(getApplicationContext(), "Connection Accepted", Toast.LENGTH_SHORT).show();
+                    textView.setText("Connected to  "+s);
+                    textView.setVisibility(View.VISIBLE);
+                    animationView.setVisibility(View.INVISIBLE);
+                    break;
+                case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                    // The connection was rejected by one or both sides.
+                    textView.setText("Could not connect to  "+s);
+                    textView.setVisibility(View.VISIBLE);
+                    animationView.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), "Rejected Connection", Toast.LENGTH_SHORT).show();
+                    break;
+                case ConnectionsStatusCodes.STATUS_ERROR:
+                    // The connection broke before it was able to be accepted.
+                    textView.setText("there was an error in connection to   "+s);
+                    textView.setVisibility(View.VISIBLE);
+                    animationView.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), "Rejected Connection", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    // Unknown status code
+            }
+        }
+
+        @Override
+        public void onDisconnected(String s) {
+            int a=4;
+        }
+    };
+    PayloadCallback mPayLoadCallback= new PayloadCallback() {
+        @Override
+        public void onPayloadReceived(String s, Payload payload) {
+            Toast.makeText(getApplicationContext(),"Payload Received",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPayloadTransferUpdate(String s, PayloadTransferUpdate payloadTransferUpdate) {
+            Toast.makeText(getApplicationContext(),"Payload Updated",Toast.LENGTH_SHORT).show();
+        }
+    };
 
 }
